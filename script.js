adba6090ddb107fc0c1cc0f22b34e865c639d018
Daria <spodina.darya@gmail.com>
2022-11-08 21:06:25 +0100

    updates

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let apiKey = "3fdfe9a6806b9f43111a0155e65cce0f";
 let city = document.querySelector(".city");
 let button = document.querySelector("button");
+let weatherByDay = {};
 button.onclick = function () {
   if (city.value) {
     let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city.value}&limit=1&appid=${apiKey}`;
@@ -15,36 +16,38 @@ button.onclick = function () {
             .then((j) => j.json())
             .then((data) => {
               let container = document.querySelector(".weatherForecast");
-              // document.querySelector(".cityName").innerHTML = city.value;
-              // document.querySelector(".temp").innerHTML = main.temp;
-              // document.querySelector(".desc").innerHTML = weather[0]['description'];
+              document.querySelector(".cityName").innerHTML = city.value;
               data.list.forEach((element) => {
-                let card = renderWeatherCard(element);
-                container.append(card);
+                let day = element.dt_txt.substring(0, 10);
+                if (day in weatherByDay) {
+                  weatherByDay[day].push(element);
+                } else {
+                  weatherByDay[day] = [element];
+                }
+                // let card = renderWeatherCard(element);
               });
+              console.log(weatherByDay)
+              for(let day in weatherByDay){
+                let card =  renderWeatherDay(day, weatherByDay[day])
+                container.append(card);
+              }
             });
         }
       });
   }
 };
-function renderWeatherCard(item) {
+function renderWeatherDay (day, weatherList){
+let listOfTemp = weatherList.map(item => 
+  item.main.temp 
+  )
+  let minTemp = Math.min(...listOfTemp)
+  let maxTemp = Math.max(...listOfTemp)
   let holder = document.createElement("div");
   holder.className = "holder";
-  holder.innerHTML = `tempeture is ${Math.round(item.main.temp)}&deg; and it is ${item.weather[0].description}`;
+  holder.innerHTML = `tempeture is ${Math.round(minTemp)} - ${Math.round(maxTemp)}`
   return holder;
+  console.log(listOfTemp)
 }
-// let apiKey = "3fdfe9a6806b9f43111a0155e65cce0f";
-// let city = document.querySelector(".city");
-// fetch(
-//   "http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}"
-// )
-//   .then(function (resp) {return resp.json() })
-//   .then(function (data) {
-//     document.querySelector(".cityName").innerHTML = city.value;
-//     document.querySelector(".temp").innerHTML = data.main.temp;
-//     document.querySelector(".desc").innerHTML = data.weather[0]["description"];
-//     console.log(data);
-//   });
-// .catch(function(){
-
-// })
+// function renderWeatherCard(item) {
+//   )}&deg; and it is ${item.weather[0].description}`;
+// }
