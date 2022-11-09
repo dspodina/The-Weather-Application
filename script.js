c4c9c61560c052eae98bca3f724b1feb58508ac5
Daria <spodina.darya@gmail.com>
2022-11-09 15:24:49 +0100

    updates

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let apiKey = "3fdfe9a6806b9f43111a0155e65cce0f";
 let city = document.querySelector(".city");
 let button = document.querySelector("button");
-let weatherByDay = {};
+
 button.onclick = function () {
   if (city.value) {
     let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city.value}&limit=1&appid=${apiKey}`;
@@ -15,8 +15,20 @@ button.onclick = function () {
           fetch(weatherUrl)
             .then((j) => j.json())
             .then((data) => {
-              let container = document.querySelector(".weatherForecast");
-              document.querySelector(".cityName").innerHTML = city.value;
+              let weatherByDay = {};
+              let container = document.querySelector(
+                `.weatherForecast.${city.value}`
+              );
+              if (!container) {
+                container = document.createElement("div");
+                container.className = `weatherForecast ${city.value}`
+                container.innerHTML = `
+<p class="cityName">${city.value}</p>
+<p class="forecastList"></p>`;
+                document.querySelector(".container").append(container);
+              }
+              let forecastList = container.querySelector(".forecastList");
+              city.value = "";
               data.list.forEach((element) => {
                 let day = element.dt_txt.substring(0, 10);
                 if (day in weatherByDay) {
@@ -26,28 +38,29 @@ button.onclick = function () {
                 }
                 // let card = renderWeatherCard(element);
               });
-              console.log(weatherByDay)
-              for(let day in weatherByDay){
-                let card =  renderWeatherDay(day, weatherByDay[day])
-                container.append(card);
+              console.log(weatherByDay);
+              forecastList.innerHTML = "";
+              for (let day in weatherByDay) {
+                let card = renderWeatherDay(day, weatherByDay[day]);
+                forecastList.append(card);
               }
             });
         }
       });
   }
 };
-function renderWeatherDay (day, weatherList){
-let listOfTemp = weatherList.map(item => 
-  item.main.temp 
-  )
-  let minTemp = Math.min(...listOfTemp)
-  let maxTemp = Math.max(...listOfTemp)
+function renderWeatherDay(day, weatherList) {
+  let listOfTemp = weatherList.map((item) => item.main.temp);
+  let minTemp = Math.min(...listOfTemp);
+  let maxTemp = Math.max(...listOfTemp);
   let holder = document.createElement("div");
   holder.className = "holder";
-  holder.innerHTML = `tempeture is ${Math.round(minTemp)} - ${Math.round(maxTemp)}`
+  holder.innerHTML = `${day} tempeture is ${Math.round(minTemp)}&deg - ${Math.round(
+    maxTemp
+  )}&deg`;
   return holder;
-  console.log(listOfTemp)
+  console.log(listOfTemp);
 }
 // function renderWeatherCard(item) {
-//   )}&deg; and it is ${item.weather[0].description}`;
+//   )} and it is ${item.weather[0].description}`;
 // }
